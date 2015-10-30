7442e71b947fbfe83c58429fba482363609bb832
Francisco David Luna López <carmen-lopez-mely@hotmail.com>
2015-10-29 18:25:30 -0600

    Tarea

---
+++ ciclo_suma_tarea.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>//Libreria de E/S
+/*Tarea*/
+int i;
+int suma;
+int final;
+int main ()
+{
+	printf("Ingrese cualquier numero\n");
+	scanf("%d",&final);
+	while (i<=final)
+	{
+		printf("%d\n",i=i+1,suma=suma+i);
+	}
+	printf("el resultado es %d",i);
+}
