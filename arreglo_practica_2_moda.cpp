0b4c2c10616f80b32a257ab0420025ac6aaa5bf3
Francisco David Luna López <carmen-lopez-mely@hotmail.com>
2015-11-16 16:29:54 -0600

    PRACTICA

---
+++ arreglo_practica_2_moda.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>//Libreria de E/S
+int moda [10];
+int i;
+int main ()
+
+int moda,i,mayor=0,menor=10; 
+for (i=0;i<=10;i++)
+{
+	print("Dame una calificacion\n");
+	scanf("%d",&arreglo[i]);	
+	{
+	moda[i]=0;
+	moda[arreglo[i]-1]++;
+	while (moda<=6)
+	}
+	{
+		printf("%d"&arreglo[i]);
+		scanf("%d"&arreglo[i]);
+	}
+}
+
