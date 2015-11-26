e0fa1175cd481f69f567a0bca68cc14ea0896d54
Francisco David Luna López <carmen-lopez-mely@hotmail.com>
2015-11-25 20:00:27 -0600

    CADENAS

---
+++ cadenas_practica_2.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>//Libreria de E/S
+char cadena [1]; 
+int buscar;
+int i;
+int oso;
+int barro;
+int main ()
+{
+	printf("Barroso\n");
+	scanf("%d",cadena);
+	{
+		scanf("%d",cadena);
+		scanf("%c",&buscar);
+		for (i=0;i<1;i++)
+		{
+			if (cadena[i]==oso,barro)
+			{
+				cadena[i]=oso,barro;
+			}
+			else 
+			{
+				printf("Si existe\n");
+			}
+			if (cadena[i]==0)
+			{
+				cadena[i]=0;
+			}
+			else 
+			{
+				printf("No existe\n");
+			}
+		}
+	}
+}
